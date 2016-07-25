6344ebfd8dc453ec92bd8956869b372c1c70928d
Julien Martin <balteo@gmail.com>
2016-07-25 08:57:57 +0200

    Remove unused import

--- src/main/java/com/example/configuration/DataConfiguration.java
+++ src/main/java/com/example/configuration/DataConfiguration.java
@@ -1,12 +1,10 @@
 package com.example.configuration;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.sql.DataSource;
 
